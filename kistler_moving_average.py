f2dd9b99586c005d2ec98651021bb52710b8f587
Kirill <kkirilenkov@gmail.com>
2019-11-01 05:18:26 +0700

    did as requested

--- kistler_moving_average.py
+++ kistler_moving_average.py
@@ -1,15 +1,45 @@
-from inputpath import path_setter
 import os
 import pandas as pd
 from math import sqrt
 from datetime import datetime as dt
 
-message = 'Введите полный путь к папке с логфайлами стабилоплатформы Kistler: '
-hard_path = 'C:/Users/Kirill/Desktop/kistler/main'
-path_setter(hard_path, message=message)
-MAX_BYTES = 950000
 # only files less than MAX_BYTES:
+MAX_BYTES = 950000
+# write results to MS Excel:
+WRITE_EXCEL = True
+msg = 'Введите полный путь к папке с логфайлами стабилоплатформы Kistler: '
+hard_path = 'C:/Users/Kirill/Desktop/kistler/main'
+
+
+def path_setter(link, message=msg, stage=False):
+    if message[-1] != '\n':
+        message += '\n'
+    try:
+        os.chdir(link)
+    except FileNotFoundError:
+        if not stage:
+            print('Hard path not found')
+        else:
+            print('Cannot find the specified path')
+        path_setter(input(message), message=message, stage=True)
+
+
+path_setter(hard_path)
+
+
+def file_match(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        counter = 0
+        for line in f:
+            if line == 'abs time (s)\tCOM Px\tCOM Py\n':
+                return True
+            counter += 1
+            if counter > 25:
+                return False
+
+
 files = list(filter(lambda fl: os.path.getsize(fl) < MAX_BYTES and 'without_outliers' not in fl, os.listdir(path='.')))
+files = list(filter(lambda fl: file_match(fl), files))
 print('File list: ')
 for i in files:
     print(i)
@@ -45,14 +75,14 @@ writer = pd.ExcelWriter('../kistler_alphas_{0:s}_{1:s}_{2:s}.xlsx'.format(str(al
 
 
 def outlier_check(value, mean, std):
-    if mean + 3*std < value or mean - 3*std > value:
+    if mean + 3 * std < value or mean - 3 * std > value:
         return True
     else:
         return False
 
 
 def exp_avg(curr_val, prev_avg, alpha=alpha_avg):
-    return (1 - alpha)*prev_avg + curr_val*alpha
+    return (1 - alpha) * prev_avg + curr_val * alpha
 
 
 def exp_variance(curr_val, prev_variance, prev_avg, alpha=alpha_var):
@@ -61,11 +91,13 @@ def exp_variance(curr_val, prev_variance, prev_avg, alpha=alpha_var):
     return (1 - alpha) * (prev_variance + diff * incr)
 
 
-def main():
-    for file in files:
+def main(file_list):
+    result_df_list = []
+    for file in file_list:
         df = pd.read_csv(file, names=['Time', 'X', 'Y', 'Cue_X', 'Cue_Y'], sep='\t', skip_blank_lines=True,
                          skiprows=19, error_bad_lines=False, engine='python')
-        '''start values: classic mean & standard deviation'''
+        '''initial values: classic mean & standard deviation'''
+        print(df)
         x_avg = df.mean(axis=0).loc['X']
         y_avg = df.mean(axis=0).loc['Y']
         x_std = df.std(axis=0).loc['X']
@@ -77,18 +109,19 @@ def main():
         x_counter = 0
         y_counter = 0
 
-        deletion_key = set()
+        deletion_keys = set()
         print('----------------------------------------\n'
               'Processing {!r} ...'.format(file))
 
-        for index in range(df.__len__()):
+        ln = df.__len__()
+        for index in range(ln):
             cvx = df.loc[index, 'X']
             tx = int(outlier_check(cvx, x_avg, sqrt(x_variance)))
             x_counter += tx
             df.loc[index, 'Cue_X'] = tx
             if tx == 1:
-                deletion_key.add(index)
-                print('File {1:s}. X outlier line: {0:d}'.format(index, file))
+                deletion_keys.add(index)
+                print('File {1:s} | X outlier line: {0:d}'.format(index, file))
             x_variance = df.loc[index, 'exp_X_var'] = exp_variance(cvx, x_variance, x_avg)
             x_avg = df.loc[index, 'exp_X_avg'] = exp_avg(cvx, x_avg)
 
@@ -97,29 +130,27 @@ def main():
             y_counter += ty
             df.loc[index, 'Cue_Y'] = ty
             if ty == 1:
-                deletion_key.add(index)
-                print('File {1:s}. Y outlier line: {0:d}'.format(index, file))
+                deletion_keys.add(index)
+                print('File {1:s} | Y outlier line: {0:d}'.format(index, file))
             y_variance = df.loc[index, 'exp_Y_var'] = exp_variance(cvy, y_variance, y_avg)
             y_avg = df.loc[index, 'exp_Y_avg'] = exp_avg(cvy, y_avg)
 
         print('\nFor file {0!r},\n'
               'the number of X drop-down lines is: {1:d},\n'
-              'the number of Y drop-down lines is: {2:d}'.format(file, x_counter, y_counter))
-
-        df.drop(deletion_key, inplace=True)
+              'the number of Y drop-down lines is: {2:d}\n'
+              'Total lines: {3:d}'.format(file, x_counter, y_counter, ln))
+        result_df_list.append(pd.DataFrame([[x_counter, y_counter, ln]], columns=['X outlier lines',
+                                           'Y outlier lines', 'Total lines'], index=[file]))
+        df.drop(deletion_keys, inplace=True)
         df.drop(columns=['Cue_X', 'Cue_Y', 'exp_X_var', 'exp_Y_var', 'exp_X_avg', 'exp_Y_avg'], inplace=True)
         with open('without_outliers_alphas_' + str(alpha_avg) + '_' + str(alpha_var) + '_' + file, 'w',
                   encoding='utf-8') as f:
             f.write(df.to_string(header=True, index=False))
 
-        df.to_excel(writer, sheet_name=file[:-4], index=False)
+    if WRITE_EXCEL:
+        pd.concat(result_df_list, axis=0).to_excel(writer, sheet_name='Outliers', index=True)
         writer.save()
 
 
 if __name__ == "__main__":
-    main()
-
-
-# В эксель писать только количество аутлаеров для х и у. Подумать над методами проверки
-# для соотвтевтия размера и формата файла. А также проверять на соответвие переменным
-# скорость/сила
+    main(files)

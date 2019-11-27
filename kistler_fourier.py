b709c437042d4bc3d95ba5b0dd34f0c8d259571b
Kirill <kkirilenkov@gmail.com>
2019-11-27 23:56:42 +0700

    fft edited

--- kistler_fourier.py
+++ kistler_fourier.py
@@ -5,32 +5,37 @@ import os
 
 path = 'C:/Users/Kirill/Desktop/kistler/main'
 os.chdir(path)
-file = 'without_outliers_alphas_0.01_0.01_Kirill 004.txt'
+kis_file = path + '/' + 'without_outliers_alphas_0.01_0.01_Kirill 004.txt'
 
-dataX = np.loadtxt(path + '/' + file, skiprows=1)
-fs = 1000
 
-# Get real amplitudes of FFT (only in positive frequencies)
-fft_vals = np.absolute(np.fft.rfft(dataX))
+def main(file):
+    dataX = np.loadtxt(file, skiprows=1)[:, 1]
+    fs = 1000
 
-# Get frequencies for amplitudes in Hz
-fft_freq = np.fft.rfftfreq(len(dataX), 1.0/fs)
+    # Get real amplitudes of FFT (only in positive frequencies)
+    fft_vals = np.absolute(np.fft.rfft(dataX))
 
-# Define bands
-bands = {'0-0.3': (0.0, 0.3), '0.3-1': (0.3, 1.0), '1-7': (1.0, 7.0)}
+    # Get frequencies for amplitudes in Hz
+    fft_freq = np.fft.rfftfreq(len(dataX), 1.0/fs)
 
-# Take the mean of the fft amplitude for each band
-band_fft = dict()
-for band in bands:
-    freq_ix = np.where((fft_freq >= bands[band][0]) &
-                       (fft_freq <= bands[band][1]))[0]
-    band_fft[band] = np.mean(fft_vals[freq_ix])
+    # Define bands
+    bands = {'0-0.3': (0.0, 0.3), '0.3-1': (0.3, 1.0), '1-7': (1.0, 7.0)}
 
-df = pd.DataFrame(columns=['band', 'value'])
-df['band'] = bands.keys()
-df['value'] = [band_fft[band] for band in bands]
-ax = df.plot.bar(x='band', y='value', legend=False)
-ax.set_xlabel("Stab band")
-ax.set_ylabel("Mean band Amplitude")
-ax.plot()
-plt.show()
+    # Take the mean of the fft amplitude for each band
+    band_fft = dict()
+    for band in bands:
+        freq_ix = np.where((fft_freq >= bands[band][0]) & (fft_freq <= bands[band][1]))[0]
+        band_fft[band] = np.mean(fft_vals[freq_ix])
+
+    df = pd.DataFrame(columns=['band', 'value'])
+    df['band'] = bands.keys()
+    df['value'] = [band_fft[band] for band in bands]
+    ax = df.plot.bar(x='band', y='value', legend=False)
+    ax.set_xlabel("Stab band")
+    ax.set_ylabel("Mean band Amplitude")
+    ax.plot()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main(kis_file)

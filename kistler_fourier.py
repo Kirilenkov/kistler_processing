799de9962f43f86c1332dc9ec91b4021fa86c497
Kirill <kkirilenkov@gmail.com>
2019-11-27 23:32:21 +0700

    fft prototype

---
+++ kistler_fourier.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+path = 'C:/Users/Kirill/Desktop/kistler/main'
+os.chdir(path)
+file = 'without_outliers_alphas_0.01_0.01_Kirill 004.txt'
+
+dataX = np.loadtxt(path + '/' + file, skiprows=1)
+fs = 1000
+
+# Get real amplitudes of FFT (only in positive frequencies)
+fft_vals = np.absolute(np.fft.rfft(dataX))
+
+# Get frequencies for amplitudes in Hz
+fft_freq = np.fft.rfftfreq(len(dataX), 1.0/fs)
+
+# Define bands
+bands = {'0-0.3': (0.0, 0.3), '0.3-1': (0.3, 1.0), '1-7': (1.0, 7.0)}
+
+# Take the mean of the fft amplitude for each band
+band_fft = dict()
+for band in bands:
+    freq_ix = np.where((fft_freq >= bands[band][0]) &
+                       (fft_freq <= bands[band][1]))[0]
+    band_fft[band] = np.mean(fft_vals[freq_ix])
+
+df = pd.DataFrame(columns=['band', 'value'])
+df['band'] = bands.keys()
+df['value'] = [band_fft[band] for band in bands]
+ax = df.plot.bar(x='band', y='value', legend=False)
+ax.set_xlabel("Stab band")
+ax.set_ylabel("Mean band Amplitude")
+ax.plot()
+plt.show()

e36c99ae1677667796e4e7e6bd4d296464a15750
Kirill <kkirilenkov@gmail.com>
2019-12-23 03:25:27 +0700

    spectrum

--- kistler_fourier.py
+++ kistler_fourier.py
@@ -10,7 +10,7 @@ kis_file = path + '/' + 'without_outliers_alphas_0.01_0.01_Ksenia_EC_002.txt'
 
 
 def main(file):
-    data = np.loadtxt(file, skiprows=1)[:, 2]
+    data = np.loadtxt(file, skiprows=1)[:, 1]
     fs = 1000
     # Get real amplitudes of FFT (only in positive frequencies)
     while True:
@@ -32,14 +32,19 @@ def main(file):
 
     # Take the mean of the fft amplitude for each band
     band_fft = dict()
+    freq_ix_list = []
     for band in bands:
         freq_ix = np.where((fft_freq >= bands[band][0]) & (fft_freq <= bands[band][1]))[0]
         print("------------------------------------------------------")
+        freq_ix_list.append(freq_ix)
         max_magn_for_band = np.max(fft_vals[freq_ix])
         band_fft[band] = max_magn_for_band
         max_peak_ix = np.where(fft_vals == max_magn_for_band)
         print(fft_freq[max_peak_ix])
 
+    freq_ix_cum = np.concatenate(freq_ix_list)
+
+    """
     df = pd.DataFrame(columns=['band', 'value'])
     df['band'] = bands.keys()
     df['value'] = [band_fft[band] for band in bands]
@@ -47,6 +52,16 @@ def main(file):
     ax = df.plot.bar(x='band', y='value', legend=False)
     ax.set_xlabel("Stab band")
     ax.set_ylabel("Max band Magnitude")
+    """
+    df = pd.DataFrame(columns=['freq', 'magn'])
+    df['freq'] = [fft_freq[ix] for ix in freq_ix_cum]
+    df['magn'] = [fft_vals[ix] for ix in freq_ix_cum]
+
+    ax = df.plot.line(x='freq', y='magn', legend=False)
+    ax.set_xlabel("Frequencies [Hz]")
+    ax.set_ylabel("Magnitude")
+    print(plt.xticks())
+
     ax.plot()
     plt.show()
 

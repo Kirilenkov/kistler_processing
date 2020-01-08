3d6bff1c7f3220d2b37cb7fb369c19e98797d9a4
Kirill <kkirilenkov@gmail.com>
2020-01-08 23:10:22 +0700

    conv

--- kistler_fourier.py
+++ kistler_fourier.py
@@ -4,15 +4,38 @@ import matplotlib.pyplot as plt
 import os
 from scipy.signal import get_window
 
-path = 'C:/Users/Kirill/Desktop/kistler/main'
-os.chdir(path)
-kis_file = path + '/' + 'without_outliers_alphas_0.01_0.01_Ksenia_EC_002.txt'
+default_msg = 'Enter the full path to the folder with the log files: \n'
 
 
-def main(file):
-    data = np.loadtxt(file, skiprows=1)[:, 1]
+def path_setter(link, message=default_msg, stage=False):
+    if message[-1] != '\n':
+        message += '\n'
+    try:
+        os.chdir(link)
+    except FileNotFoundError:
+        if not stage:
+            print('Hard path not found')
+        else:
+            print('Cannot find the specified path')
+        path_setter(input(message), message=message, stage=True)
+
+
+hard_path = 'C:/Users/Kirill/Desktop/kistler/main'
+path_setter(hard_path)
+file_dir_list = [(p, f) for p, d, f in os.walk(os.getcwd())]
+file_list = []
+for p, f in file_dir_list:
+    for i in f:
+        if '.txt' in i:
+            file_list.append(p + '\\' + i)
+print(file_list)
+
+
+def core(file, axis):
+    data = np.loadtxt(file, skiprows=1)[:, axis]
     fs = 1000
     # Get real amplitudes of FFT (only in positive frequencies)
+    '''
     while True:
         trigger = input('Применить окно Хеннинга? y/n \n').lower()
         if trigger == 'y':
@@ -23,6 +46,10 @@ def main(file):
         elif trigger == 'n':
             fft_vals = np.absolute(np.fft.rfft(data))
             break
+    '''
+    m = len(data)
+    w = get_window('hann', m)
+    fft_vals = np.absolute(np.fft.rfft(data * w))
 
     # Get frequencies for amplitudes in Hz
     fft_freq = np.fft.rfftfreq(len(data), 1.0/fs)
@@ -44,15 +71,6 @@ def main(file):
 
     freq_ix_cum = np.concatenate(freq_ix_list)
 
-    """
-    df = pd.DataFrame(columns=['band', 'value'])
-    df['band'] = bands.keys()
-    df['value'] = [band_fft[band] for band in bands]
-    print(df)
-    ax = df.plot.bar(x='band', y='value', legend=False)
-    ax.set_xlabel("Stab band")
-    ax.set_ylabel("Max band Magnitude")
-    """
     df = pd.DataFrame(columns=['freq', 'magn'])
     df['freq'] = [fft_freq[ix] for ix in freq_ix_cum]
     df['magn'] = [fft_vals[ix] for ix in freq_ix_cum]
@@ -60,11 +78,20 @@ def main(file):
     ax = df.plot.line(x='freq', y='magn', legend=False)
     ax.set_xlabel("Frequencies [Hz]")
     ax.set_ylabel("Magnitude")
-    print(plt.xticks())
-
+    ax.set_title('Name: {0:s}, Axis: {1:s}'
+                 .format(file.split('/')[-1], 'X' if axis == 1 else ('Y' if axis == 2 else 'Unknown axis')))
+    ax.xaxis.set_major_locator(plt.MultipleLocator(0.5))
+    plt.grid(True)
     ax.plot()
     plt.show()
 
 
+def main(files):
+    for file in files:
+        for i in range(1, 2, 1):
+            core(file, i)
+
+
 if __name__ == '__main__':
-    main(kis_file)
+    main(file_list)
+
